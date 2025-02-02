7dbc4681c76fdc1452d68ee91be7c313d03bc2a7
MohammedRafiuddin <59638244+MohammedRafiuddin@users.noreply.github.com>
2025-02-02 15:13:11 +0530

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,7 @@
+// Navigation logic
+const mobileMenu = document.querySelector('.mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
